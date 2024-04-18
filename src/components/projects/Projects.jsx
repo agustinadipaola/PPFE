c7d9c9a26c6646b8207affcaec9d5ebe70247444
unknown <agustinadipaolauk@gmail.com>
2024-04-18 22:28:22 +0100

    some styling updates

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -11,19 +11,72 @@ import BCSWelcomeAdmin from "../../images/bcs/BCSWelcomeAdmin.PNG";
 import BCSItems from "../../images/bcs/BCSItems.PNG";
 import BCSLogIn from "../../images/bcs/BCSLogIn.PNG";
 import BCSShopItems from "../../images/bcs/BCSShopItems.PNG";
+import BCSCheckOut from "../../images/bcs/BCSItemCheckout.PNG";
+import Rose from "../../images/rose/Rose.PNG";
+import RoseAdd from "../../images/rose/RoseAdd.PNG";
+import RoseBuy from "../../images/rose/RoseBuy.PNG";
+import RoseBuyers from "../../images/rose/RoseBuyers.PNG";
+import RoseLet from "../../images/rose/RoseLet.PNG";
+import LogoBCS from "../../images/logos/BCS.png";
+import LogoPaistina from "../../images/logos/Paistina.png";
+import LogoCana from "../../images/logos/Cana.png";
+import LogoRose from "../../images/logos/Rose.png";
 
 import Carousel from "react-bootstrap/Carousel";
 
 function Projects() {
   return (
+
     <>
+     
+{/* for font style porpuses, I use  &lt; and &gt; that represent >< */}
+<h1 style={{fontFamily: "jhal", textAlign: "center"}}>&lt; Projects &gt;</h1>
       {" "}
-      <div style={{ display: "flex" }}>
-        <Card
-          className="col-sm-6 col-md-4 col-lg-6 m-1"
+      <div style={{ display: 'flex', flexWrap: 'wrap'}}>        <Card
+          className="col-sm-6 col-md-6 col-lg-6"
           style={{ textAlign: "center" }}
         >
-          <Card.Text>
+          <Card.Text>   
+            The primary objective of this open-source project was to enhance the
+            provided open-source code by integrating additional features and
+            improvements, aligning with evolving user requirements and industry
+            standards
+            <br />
+            <br />
+          </Card.Text>
+          <Carousel interval={null} >
+          
+            <Carousel.Item>
+              <Card.Img variant="bottom" src={ProInc2} sizes={10} />
+            </Carousel.Item>
+            <Carousel.Item>
+              <Card.Img variant="bottom" src={BCSWelcomeAdmin} sizes={10} />
+            </Carousel.Item>
+            <Carousel.Item>
+              <Card.Img variant="bottom" src={BCSCreateItem} sizes={10} />
+            </Carousel.Item>
+            <Carousel.Item>
+              <Card.Img variant="bottom" src={BCSItems} sizes={10} />
+            </Carousel.Item>
+            <Carousel.Item>
+              <Card.Img variant="bottom" src={BCSLogIn} sizes={10} />
+            </Carousel.Item>
+            <Carousel.Item>
+              <Card.Img variant="bottom" src={BCSShopItems} sizes={10} />
+            </Carousel.Item>
+            <Carousel.Item>
+              <Card.Img variant="bottom" src={BCSCheckOut} sizes={10} />
+            </Carousel.Item>
+          </Carousel>
+
+          <Card.Body></Card.Body>
+        </Card>
+
+        <Card
+          className="col-sm-6 col-md-6 col-lg-6 "
+          style={{ textAlign: "center" }}
+        >
+          <Card.Text >
             The primary objective of this software coding project was to
             modernize an existing legacy Java-based application by introducing a
             contemporary tech stack, specifically utilizing Spring for the
@@ -31,7 +84,7 @@ function Projects() {
             technologies.
           </Card.Text>
 
-          <Carousel >
+          <Carousel interval={null} >
             <Carousel.Item>
               <Card.Img variant="bottom" src={ProInc1} sizes={10} />
             </Carousel.Item>
@@ -55,40 +108,41 @@ function Projects() {
           <Card.Body></Card.Body>
           <br />
         </Card>
+      
+      
         <Card
-          className="col-sm-6 col-md-4 col-lg-6 m-1"
+          className="col-sm-6 col-md-6 col-lg-6"
           style={{ textAlign: "center" }}
         >
           <Card.Text>
-            The primary objective of this open-source project was to enhance the
-            provided open-source code by integrating additional features and
-            improvements, aligning with evolving user requirements and industry
-            standards
-            <br />
-            <br />
+            The primary objective of this software coding project was to
+            modernize an existing legacy Java-based application by introducing a
+            contemporary tech stack, specifically utilizing Spring for the
+            backend, React for the frontend, and other complementary frontend
+            technologies.
           </Card.Text>
-          <Carousel >
+
+          <Carousel interval={null} >
             <Carousel.Item>
-              <Card.Img variant="bottom" src={ProInc2} sizes={10} />
+              <Card.Img variant="bottom" src={Rose} sizes={10} />
             </Carousel.Item>
             <Carousel.Item>
-              <Card.Img variant="bottom" src={BCSWelcomeAdmin} sizes={10} />
+              <Card.Img variant="bottom" src={RoseAdd} sizes={10} />
             </Carousel.Item>
             <Carousel.Item>
-              <Card.Img variant="bottom" src={BCSCreateItem} sizes={10} />
+              <Card.Img variant="bottom" src={RoseBuy} sizes={10} />
             </Carousel.Item>
             <Carousel.Item>
-              <Card.Img variant="bottom" src={BCSItems} sizes={10} />
+              <Card.Img variant="bottom" src={RoseBuyers} sizes={10} />
             </Carousel.Item>
             <Carousel.Item>
-              <Card.Img variant="bottom" src={BCSLogIn} sizes={10} />
+              <Card.Img variant="bottom" src={RoseLet} sizes={10} />
             </Carousel.Item>
-            <Carousel.Item>
-              <Card.Img variant="bottom" src={BCSShopItems} sizes={10} />
-            </Carousel.Item>
+            
           </Carousel>
 
           <Card.Body></Card.Body>
+          <br />
         </Card>
       </div>
     </>
